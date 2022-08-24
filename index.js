024cb836b595fd3b91ed6ad5dd2d85caa7b60569
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-08-25 00:22:00 +0200

    config

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  rules: {
+    '@typescript-eslint/no-shadow': ['error'],
+    curly: ['warn'],
+    'dot-notation': ['warn'],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+  },
+};
